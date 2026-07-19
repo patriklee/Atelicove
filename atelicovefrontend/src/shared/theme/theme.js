4f116e762683bad2f1fc2d6b8a457c2be8915aea
Patrick Lee <patrick.lee386@gmail.com>
2026-07-19 01:51:40 -0500

    fix: Added an icon wrapper and theme wrapper

---
+++ atelicovefrontend/src/shared/theme/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: "Inter, sans-serif",
+    },
+
+    palette: {
+        primary: {
+            main: "#5B7C99",
+        },
+    },
+});
+
+export default theme;
