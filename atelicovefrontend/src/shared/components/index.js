a6b671ae90f5432920a14dedb1604cb100748802
Patrick Lee <patrick.lee386@gmail.com>
2026-07-22 20:08:30 -0500

    Continue chore.

--- atelicovefrontend/src/shared/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as EmptyState } from './EmptyState';
-export { default as ErrorState } from './ErrorState';
-export { default as LoadingState } from './LoadingState';
-export { default as PageSection } from './PageSection';
--- atelicovefrontend/src/shared/layout/index.js
+++
@@ -1,1 +0,0 @@
-export { default as StudioLayout } from './StudioLayout';
